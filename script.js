b1beaa643db55cadbf58daa89d8125b741d7ceea
webermg <matthew.gerald.weber@gmail.com>
2020-09-10 19:12:34 -0700

    added definition of generatePassword function and helper functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 let checkboxes = document.querySelectorAll(".checkbox");
+
 let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "0123456789";
@@ -8,12 +9,34 @@ let specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let datasets = gatherDatasets();
+  let passLength = document.getElementById("lengthInput").value;
+  var password = generatePassword(datasets, passLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+// Generates a password based on the criteria specified
+const generatePassword = (datasets, passLength) => {
+  let password = "";
+  for(let i = 0; i < passLength; i++) {
+    let setNum = Math.floor(Math.random() * datasets.length);
+    let charNum = Math.floor(Math.random() * datasets[setNum].length);
+    password += datasets[setNum][charNum];
+  }
+  return password;
+};
+
+const gatherDatasets = () => {
+  let datasets = [];
+  if(document.getElementById("lowercaseCB").checked) datasets.push(lowerCaseChars);
+  if(document.getElementById("uppercaseCB").checked) datasets.push(upperCaseChars);
+  if(document.getElementById("numberCB").checked) datasets.push(numbers);
+  if(document.getElementById("specialCharCB").checked) datasets.push(specialChars);
+  return datasets;
+};
 
 // Verifies that at least one checkbox is checked and disables the button if none are
 const verifyFormValidity = () => {
