33267c018164ca48b6230a37eb267d5d5b1432b4
webermg <matthew.gerald.weber@gmail.com>
2020-09-10 18:48:11 -0700

    added function to verify inputs are valid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let checkboxes = document.querySelectorAll(".checkbox");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +11,20 @@ function writePassword() {
 
 }
 
+// Verifies that at least one checkbox is checked and disables the button if none are
+const verifyFormValidity = () => {
+  let atLeastOneChecked = false;
+  checkboxes.forEach(element => {
+    if(element.checked) atLeastOneChecked = true;
+  })
+  if(atLeastOneChecked) generateBtn.disabled = false;
+  else generateBtn.disabled = true;
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to checkboxes
+checkboxes.forEach(element => {
+  element.addEventListener("click", verifyFormValidity)
+});
